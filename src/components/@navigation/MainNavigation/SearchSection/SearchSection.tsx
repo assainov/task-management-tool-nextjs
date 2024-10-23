edb0e5de26fa01e7329749dc1a4a296d667f9f17
assainov <iliyas.assainov@gmail.com>
2024-10-24 00:48:46 +0400

    Fix localization string

--- src/components/@navigation/MainNavigation/SearchSection/SearchSection.tsx
+++ src/components/@navigation/MainNavigation/SearchSection/SearchSection.tsx
@@ -1,19 +1,23 @@
 import { Input } from 'components/@common/Input';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const SearchSection = () => (
-  <div className="basis-[300px]">
-    <Input
-      type="text"
-      placeholder="Search tasks"
-      endIcon={(
-        <kbd className="pointer-events-none absolute inset-y-2 right-2 rtl:left-2 rtl:right-auto hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-          <span className="text-xs">⌘</span>
-          K
-        </kbd>
+const SearchSection = () => {
+  const { t } = useTranslation('common');
+  return (
+    <div className="basis-[300px]">
+      <Input
+        type="text"
+        placeholder={t('search_tasks')}
+        endIcon={(
+          <kbd className="pointer-events-none absolute inset-y-2 right-2 rtl:left-2 rtl:right-auto hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+            <span className="text-xs">⌘</span>
+            K
+          </kbd>
           )}
-    />
-  </div>
-);
+      />
+    </div>
+  );
+};
 
 export default SearchSection;
